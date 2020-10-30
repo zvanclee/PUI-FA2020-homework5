830851cc327b7963531df3782883ce6efc1626a4
zvanclee <zachvancleef@gmail.com>
2020-10-30 15:05:42 -0400

    Update myScripts.js

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,14 +1,3 @@
-// $(document).ready(function() {
-//
-//   $('.color-choice input').on('click', function() {
-//       let colorSelection = $(this).attr('data-image');
-//
-//       $('.active').removeClass('active');
-//       $('.left-column img[data-image = ' + colorSelection + ']').addClass('active');
-//       $(this).addClass('active');
-//   });
-// });
-
 function changeColor(newColor) {
   let newProductImg = document.getElementById("product-img");
 
