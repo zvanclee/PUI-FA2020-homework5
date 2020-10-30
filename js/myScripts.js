026730e0e39895b9d1205463e292c4835e7b17ba
zvanclee <zachvancleef@gmail.com>
2020-10-30 14:35:04 -0400

    desc

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,3 +1,13 @@
 <script>
+$(document).ready(function() {
 
+  $('.color-choose input').on('click', function() {
+      var headphonesColor = $(this).attr('data-image');
+
+      $('.active').removeClass('active');
+      $('.left-column img[data-image = ' + headphonesColor + ']').addClass('active');
+      $(this).addClass('active');
+  });
+
+});
 </script>
