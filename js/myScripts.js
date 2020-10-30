f81306d31d0ac04f34595c9fb3828d67d87c4fe8
zvanclee <zachvancleef@gmail.com>
2020-10-30 15:01:27 -0400

    desc

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,10 +1,27 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+//
+//   $('.color-choice input').on('click', function() {
+//       let colorSelection = $(this).attr('data-image');
+//
+//       $('.active').removeClass('active');
+//       $('.left-column img[data-image = ' + colorSelection + ']').addClass('active');
+//       $(this).addClass('active');
+//   });
+// });
 
-  $('.color-choice input').on('click', function() {
-      let colorSelection = $(this).attr('data-image');
+function changeColor(newColor) {
+  let newProductImg = document.getElementById("product-img");
 
-      $('.active').removeClass('active');
-      $('.left-column img[data-image = ' + colorSelection + ']').addClass('active');
-      $(this).addClass('active');
-  });
-});
+  if(newColor == "strawberry") {
+    newProductImg.src = "images/strawberry-harness.png";
+  }
+  else if (newColor == "blackberry") {
+    newProductImg.src = "images/blackberry-harness.png";
+  }
+  else if (newColor == "crazyberry") {
+    newProductImg.src = "images/crazyberry-harness.png";
+  }
+  else if (newColor == "fireOrange") {
+    newProductImg.src = "images/fireOrange-harness.png";
+  }
+}
